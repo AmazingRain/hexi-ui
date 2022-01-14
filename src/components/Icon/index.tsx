285780f9153d5e6c5df81da8ec33136a83759d56
AmazingRain <muziyu@muziyudeMBP.lan>
2022-01-14 19:06:04 +0800

    icon doc done

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -21,20 +21,20 @@ interface IIconProps {
    * 可以这样写属性描述
    * @description       也可以显式加上描述名
    * @description.zh-CN 图标长度
-   * @default           --
+   * @default           16
    */
   width?: number;
   /**
    * 可以这样写属性描述
    * @description       也可以显式加上描述名
    * @description.zh-CN 图标宽度
-   * @default           --
+   * @default           16
    */
   height?: number;
   className?: string;
 }
 const Icon: React.FC<IIconProps> = (props) => {
-  const { name, color = '#333', width = 16, height = 16, className } = props;
+  const { name, color, width = 16, height = 16, className } = props;
   return (
     <svg className={className} fill={color} width={width} height={height}>
       <use xlinkHref={`${svgs}#${name}`}></use>
